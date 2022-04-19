93ab2a38a2ec63798b6895938b0e8eb4a3f2a44c
mevlutcantuna <mttuna90@gmail.com>
2022-04-19 11:23:14 +0300

    project is over

--- api/instance.js
+++ api/instance.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL:"http://localhost:3000/api/"
-})
-
-//     baseURL:"https://next-fullstack-blog.vercel.app/api/"
+  baseURL: "https://next-fullstack-blog.vercel.app/api/",
+});
