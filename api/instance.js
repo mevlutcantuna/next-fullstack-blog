e79d27d18911de2c70ea9385ef4be1a0e2a97136
mevlutcantuna <mttuna90@gmail.com>
2022-06-01 15:39:53 +0300

    fix: base api fixed

--- api/instance.js
+++ api/instance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const baseUrl = process.env.production
-  ? "https://next-fullstack-blog.vercel.app/api/"
-  : "http://localhost:3000/api/";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://next-fullstack-blog.vercel.app/api/"
+    : "http://localhost:3000/api/";
 
 export const instance = axios.create({
   baseURL: baseUrl,
